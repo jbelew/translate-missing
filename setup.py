02e9f2121bd0167149c90b5f4c5db32a0c67e58f
John Belew <john.belew@gmail.com>
2025-10-19 09:30:13 -0700

    fix: Correct a typo in the README

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import os
 from setuptools import setup, find_packages
 
+__version__ = '0.1.0'
+
 setup(
     name='translate-missing',
-    version='0.1.0',
+    version=__version__,
     packages=find_packages(),
     install_requires=[
         'deep_translator',
